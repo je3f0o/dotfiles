de6a7c8e8efe463ea27b30ddfab1665cd1c00c16
je3f0o <je3f0o@gmail.com>
2021-12-08 17:48:54 +0800

    updated

--- bash/plugins/bash/globals.plugin.sh
+++ bash/plugins/bash/globals.plugin.sh
@@ -2,6 +2,6 @@
 function update_dotfiles {
   pushd ~/cloud/dotfiles
   git remote update origin --prune || exit 1
-  source ~/.bash_profile
+  source ~/.bash/plugins/reload_bashrc/reload_bashrc.plugin.sh && reload_bashrc
   popd
 }
