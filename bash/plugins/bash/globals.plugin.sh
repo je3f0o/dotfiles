ef70e2185aaeaa7bd14770971ba4b99008b32a5e
je3f0o <je3f0o@gmail.com>
2021-12-08 17:26:35 +0800

    updated

--- bash/plugins/bash/globals.plugin.sh
+++ bash/plugins/bash/globals.plugin.sh
@@ -2,6 +2,6 @@
 function update_dotfiles {
   pushd ~/cloud/dotfiles
   git remote update origin --prune || exit 1
-  __is_darwin && source ~/.bash_profile || source ~/.bashrc
+  source ~/.bash_profile
   popd
 }
