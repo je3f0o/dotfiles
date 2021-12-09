335a1ee7286cfcee41dd72247502fba8fc45d2da
je3f0o <je3f0o@gmail.com>
2021-12-09 13:08:02 +0800

    updated

--- installer/setup.sh
+++ installer/setup.sh
@@ -1,24 +1,28 @@
 # There is a builtin variable called OSTYPE
 # legacy var depricated
-JEEFO_ENV_OS_NAME=`uname`
+JEEFO_ENV_OS_NAME=$(uname)
 
 source installer/installer.sh
 source installer/linker.sh
 
 # Toys
 if __is_darwin; then
+  $(tree --version &> /dev/null) || brew install tree
+  $(watch --version &> /dev/null) || brew install watch
   $(lolcat --version &> /dev/null) || brew install lolcat
   $(cowsay hello > /dev/null) || brew install cowsay
   $(fortune -n 0 &> /dev/null) || brew install fortune
 else
+  $(tree --version &> /dev/null) || sudo apt-get install -y tree
+  $(watch --version &> /dev/null) || sudo apt-get install -y watch
   $(lolcat --version &> /dev/null) || sudo apt-get install -y lolcat
   $(cowsay hello > /dev/null) || sudo apt-get install -y cowsay
   $(fortune -n 0 &> /dev/null) || sudo apt-get install -y fortune
+fi
 
-  if ! test -f /usr/share/cowsay/cows/small.cow; then
-    sudo ln -s /usr/share/cowsay/cows/default.cow \
-               /usr/share/cowsay/cows/small.cow
-  fi
+if ! test -f /usr/share/cowsay/cows/small.cow; then
+  sudo ln -s /usr/share/cowsay/cows/default.cow \
+             /usr/share/cowsay/cows/small.cow
 fi
 
 __jeefo_install
